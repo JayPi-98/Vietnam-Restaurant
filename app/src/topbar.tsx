5657570e182ff141dc397d5f2e4803b95ba9b14c
JayPi-98 <jpmoreno98@gmail.com>
2025-02-24 17:24:51 -0600

    Basic React App with Vite

---
+++ app/src/topbar.tsx
@@ -0,0 +1,9 @@
+
+
+export function Topbar() {
+  return (
+    <div className="flex bg-blue-500 text-white p-4 w-full fixed top-0 left-0 z-50 justify-center items-center">
+      <h1 className="italic">LaConChinChine</h1>
+    </div>
+  );
+}
